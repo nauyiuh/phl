a6e5412a3e4a9f1227d56fc0aefd875fa318459d
nauyiuh <39545224+nauyiuh@users.noreply.github.com>
2020-06-20 17:22:35 -0700

    Update test.js

--- src/phl/test.js
+++ src/phl/test.js
@@ -11,7 +11,7 @@
 		version: `0.0.1`
   };
   
- room.onPlayerBallKick = () => {
+ room.onGameStart = () => {
 	//var players = room.getPlayerList();
 	room.sendChat('Testing text');
 	//players.forEach (element => room.sendChat(element.name);
