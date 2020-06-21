82a00e7585e069c3aad3f3c5386c973fbb415230
nauyiuh <39545224+nauyiuh@users.noreply.github.com>
2020-06-20 18:02:29 -0700

    Update test.js

--- src/phl/test.js
+++ src/phl/test.js
@@ -10,24 +10,26 @@
 		author: `hk`,
 		version: `0.0.1`
 		config: {},
-		dependencies: [`sav/roles`]
   };
   
-let blueTeamStreak = 0;
-let redTeamStreak = 0;
+// If there are no admins left in the room give admin to one of the remaining players.
+function updateAdmins() { 
+  // Get all players except the host (id = 0 is always the host)
+  var players = room.getPlayerList().filter((player) => player.id != 0 );
+  if ( players.length == 0 ) return; // No players left, do nothing.
+  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
+  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
+}
 
-function onTeamVictory(scores) {
-  if (scores.red > scores.blue) {
-    blueTeamStreak = 0;
-    redTeamStreak++;
-    room.sendAnnouncement(`Red team has won ${redTeamStreak} games in a row!`, null, 0xE56E56);
-  } else {
-    redTeamStreak = 0;
-    blueTeamStreak++;
-    room.sendAnnouncement(`Blue team has won ${blueTeamStreak} games in a row!`, null, 0x5689E5);
-  }
+function onPlayerJoin(player) {
+  updateAdmins();
+}
+
+function onPlayerLeave(player) {
+  updateAdmins();
 }
 
 room.onRoomLink = function onRoomLink() {
-  room.onTeamVictory = onTeamVictory;
+  room.onPlayerLeave = onPlayerLeave;
+  room.onPlayerJoin = onPlayerJoin;
 }
