3f3bae0e700b88b1b756a22d626f2024d78aea1a
nauyiuh <39545224+nauyiuh@users.noreply.github.com>
2020-07-02 00:09:13 -0700

    Update always-one-admin.js

--- src/phl/always-one-admin.js
+++ src/phl/always-one-admin.js
@@ -1,41 +1,30 @@
-/**
- * Plugin that ensures there is always one admin in the room
- * for Headless Haxball Manager (HHM).
- * 
- * The plugin gives admins to first player that join and passes the
- * admin to someone else if the only admin leaves.
- * 
- * Kicks admins that are afk if `kickAfk` is set to true.
- * 
- * Code has been ripped off from the Haxball Headless Wiki.
- * https://github.com/haxball/haxball-issues/wiki/Headless-Host
- */
+
 
 let room = HBInit();
 room.pluginSpec = {
-  name: `phl/always-one-admin`,
-  author: `salamini`,
-  version: `1.0.0`,
+	name: 'hk/badmin',
+	author: `hk`,
+	version: `1.0.0`,
+	config: {
+		pass: ""
+	},
+	dependencies: [
+		'sav/commands'
+	]
 };
 
-// If there are no admins left in the room give admin to one of the remaining players.
-function updateAdmins() { 
-  // Get all players except the host (id = 0 is always the host)
-  var players = room.getPlayerList().filter((player) => player.id != 0 );
-  if ( players.length == 0 ) return; // No players left, do nothing.
-  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
-  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
+
+room.onCommand_clearbans = (player) => {
+	if (player.admin == true) {
+		room.clearBans();
+		room.sendAnnouncement("Bans cleared", player.id);
+	}
+	return false
 }
 
-function onPlayerJoin(player) {
-  updateAdmins();
-}
-
-function onPlayerLeave(player) {
-  updateAdmins();
-}
-
-room.onRoomLink = function onRoomLink() {
-  room.onPlayerLeave = onPlayerLeave;
-  room.onPlayerJoin = onPlayerJoin;
+room.onCommand_auth = (player, args) => {
+	if (args[0] == room.getConfig().pass) {
+		room.setPlayerAdmin(player.id, true);
+	}
+	return false
 }
